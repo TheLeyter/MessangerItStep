b63e8564531c9004ec85b133f6c128980875bb22
Stanisław Rudnicki <rudnicki@list.ru>
2021-12-08 18:28:06 +0200

    Chat fragment added

---
+++ app/src/main/java/ua/leyter/messengeritstep/screens/chat/ChatMessage.kt
@@ -0,0 +1,6 @@
+package ua.leyter.messengeritstep.screens.chat
+
+data class ChatMessage (
+    val content: String,
+    val isOut: Boolean
+)
---
+++ app/src/main/java/ua/leyter/messengeritstep/screens/chat/CustomRecyclerAdapter.kt
@@ -0,0 +1,34 @@
+package ua.leyter.messengeritstep.screens.chat
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import ua.leyter.messengeritstep.R
+import ua.leyter.messengeritstep.domain.entities.User
+
+class CustomRecyclerAdapter(private val users: List<User>) :
+    RecyclerView.Adapter<TwoLineItemViewHolder>() {
+
+    private val imageDownloader = ImageDownloader()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TwoLineItemViewHolder {
+        val itemView =
+            LayoutInflater.from(parent.context)
+                .inflate(R.layout.material_list_item_two_line, parent, false)
+        return TwoLineItemViewHolder(itemView)
+    }
+
+    override fun onBindViewHolder(holder: TwoLineItemViewHolder, position: Int) {
+        holder.firstLine?.text = users[position].UserName
+//        holder.secondLine?.text = "Last message bla bla bla..."
+
+        users[position].UserImage?.let {
+            imageDownloader.setImage("https://i.pravatar.cc/150?u=$it", holder.image)
+            holder.secondLine?.text = it
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return users.size
+    }
+}
